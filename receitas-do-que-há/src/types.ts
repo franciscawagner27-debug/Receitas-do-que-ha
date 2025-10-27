d9dbb97367c165bcccc461e744053f697a83987e
franciscawagner27-debug <franciscawagner27@gmail.com>
2025-10-27 17:10:19 +0000

    Add files via upload

---
+++ receitas-do-que-há/src/types.ts
@@ -0,0 +1,10 @@
+export interface Recipe {
+  id?: string
+  title: string
+  image?: string
+  ingredients: string[]
+  timeMinutes?: number
+  time_minutes?: number
+  tags?: string[]
+  steps?: string[]
+}
