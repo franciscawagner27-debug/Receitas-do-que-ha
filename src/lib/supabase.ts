2b90c960d1a036b0767e753fdc4dbe3c680de6de
franciscawagner27-debug <franciscawagner27@gmail.com>
2025-11-15 16:39:16 +0000

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,18 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  global: {
-    fetch: (url, options) => {
-      return fetch(url, {
-        ...options,
-        cache: "no-store",        // 👈 MUITO IMPORTANTE
-        headers: {
-          ...(options?.headers || {}),
-          "cache-control": "no-store", // 👈 Força a ignorar caches externos
-          "pragma": "no-cache"
-        }
-      })
-    }
-  }
-})
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
