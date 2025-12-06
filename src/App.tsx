81ed153ea9f331f4ede497bf0152683c066ccc33
franciscawagner27-debug <franciscawagner27@gmail.com>
2025-12-06 14:29:38 +0000

    Add route for CozinharPage

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import EditRecipe from "./pages/EditRecipe";
 import { smartSearch } from "./utils/smartSearch";
 import RecipePage from "./pages/RecipePage";
 import DiasSemTempoPage from "./pages/DiasSemTempoPage";
+import CozinharPage from "./pages/cozinhar";
 
 /* -------------------------------------------------------------------------- */
 /*                                   ROOT                                     */
@@ -30,6 +31,7 @@ export default function App() {
       <Route path="/admin/edit/:id" element={<EditRecipe />} />
       <Route path="/receita/:id" element={<RecipePage />} />
       <Route path="/dias-sem-tempo" element={<DiasSemTempoPage />} />
+      <Route path="/cozinhar/:id" element={<CozinharPage />} />
     </Routes>
   );
 }
