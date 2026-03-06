bf4a30c69eb4f18dd78870ebf11d52d9cf0d387b
franciscawagner27-debug <franciscawagner27@gmail.com>
2026-03-06 12:25:09 +0000

    Adjust padding in Header button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -57,7 +57,7 @@ const handleClick = (category: string) => {
   {/* LOGO */}
   <button
     onClick={() => (window.location.href = "/")}
-   className="flex items-center flex-shrink-0 pl-6 mr-2"
+   className="flex items-center flex-shrink-0 pl-10 mr-2"
     aria-label="Ir para a página inicial"
   >
     <img
