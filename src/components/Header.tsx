2e1b2ccc3d335cf1ebc8e894ca38eb6908b355cc
franciscawagner27-debug <franciscawagner27@gmail.com>
2026-03-06 12:47:13 +0000

    Adjust image translation on Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -63,7 +63,7 @@ const handleClick = (category: string) => {
    <img
   src="/icons/icon-512.png"
   alt="Receitas do Que Há"
-  className="h-24 w-24 sm:h-28 sm:w-28 md:h-32 md:w-32 object-contain notranslate translate-x-10"
+  className="h-24 w-24 sm:h-28 sm:w-28 md:h-32 md:w-32 object-contain notranslate translate-x-12"
 />
   </button>
 
