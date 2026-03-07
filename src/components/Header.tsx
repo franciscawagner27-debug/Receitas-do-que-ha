bc88ef6f6953198597d0ba6f6a66d9b8aa94afad
franciscawagner27-debug <franciscawagner27@gmail.com>
2026-03-07 11:43:45 +0000

    Increase header icon size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -63,7 +63,7 @@ const handleClick = (category: string) => {
 <img
   src="/icons/icon-512.png"
   alt="Receitas do Que Há"
- className="h-40 w-40 md:h-32 md:w-32 object-contain notranslate md:translate-x-10"
+ className="h-48 w-48 md:h-32 md:w-32 object-contain notranslate md:translate-x-10"
 />
   </button>
 
